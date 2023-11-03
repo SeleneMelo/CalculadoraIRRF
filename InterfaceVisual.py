0e1b234ba4111cfb9fdcadda08274c436bf12270
Edworldinho <64818232+Edworldinho@users.noreply.github.com>
2023-11-03 16:50:26 -0300

    Update InterfaceVisual.py

--- InterfaceVisual.py
+++ InterfaceVisual.py
@@ -197,7 +197,7 @@ def interface_visual():
             salario_base_de_calculo_list = list(range(anos_vigencia_aux))
             total_deducoes_list          = list(range(anos_vigencia_aux))
 
-            salario_liquido_list, irrf_recolhido_list, salario_base_list, adicoes_list, salario_base_de_calculo_list, total_deducoes_list = preparar_graficos(salario_liquido, irrf_recolhido, adicoes, salario_base_de_calculo, total_deducoes)
+            salario_liquido_list, irrf_recolhido_list, salario_base_list, adicoes_list, salario_base_de_calculo_list, total_deducoes_list = logicacalculadora.preparar_graficos(salario_liquido, irrf_recolhido, salario_base, adicoes, salario_base_de_calculo, total_deducoes)
 
             grafico = Grafico(salario_liquido_list, irrf_recolhido_list, anos_vigencia, -1,
                               salario_base_list, adicoes_list,
@@ -226,7 +226,7 @@ def interface_visual():
 
             salario_base, salario_bruto, salario_base_de_calculo, anos_vigencia, salario_liquido, irrf_recolhido, aliquota_ = logicacalculadora.enviardadospararelatorio()
 
-            salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido, aliquota_ = preparar_relatorio(salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido)
+            salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido, aliquota_ = logicacalculadora.preparar_relatorio(salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido)
 
             relatoriopdf = RelatorioPDF(salario_base, salario_bruto, salario_base_de_calculo,
                 anos_vigencia,
@@ -236,73 +236,6 @@ def interface_visual():
             relatoriopdf.gerarrelatorio()
 
     window_visual_interface.close()
-
-def preparar_graficos(salario_liquido, irrf_recolhido, adicoes, salario_base_de_calculo, total_deducoes):
-    
-    salario_liquido_list         = list(range(anos_vigencia_aux))
-    irrf_recolhido_list          = list(range(anos_vigencia_aux))
-    salario_base_list            = list(range(anos_vigencia_aux))
-    adicoes_list                 = list(range(anos_vigencia_aux))
-    salario_base_de_calculo_list = list(range(anos_vigencia_aux))
-    total_deducoes_list          = list(range(anos_vigencia_aux))
-
-    if(anos_vigencia_aux == 1):
-
-        salario_liquido_list[0]         = salario_liquido         * (mes_fim - mes_inicio + 1)
-        irrf_recolhido_list[0]          = irrf_recolhido          * (mes_fim - mes_inicio + 1)
-        salario_base_list[0]            = salario_base            * (mes_fim - mes_inicio + 1)
-        adicoes_list[0]                 = adicoes                 * (mes_fim - mes_inicio + 1)
-        salario_base_de_calculo_list[0] = salario_base_de_calculo * (mes_fim - mes_inicio + 1)
-        total_deducoes_list[0]          = total_deducoes          * (mes_fim - mes_inicio + 1)
-
-    elif(anos_vigencia_aux > 1):
-
-        salario_liquido_list[0]         = salario_liquido         * (12 - mes_inicio + 1)
-        irrf_recolhido_list[0]          = irrf_recolhido          * (12 - mes_inicio + 1)
-        salario_base_list[0]            = salario_base            * (12 - mes_inicio + 1)
-        adicoes_list[0]                 = adicoes                 * (12 - mes_inicio + 1)
-        salario_base_de_calculo_list[0] = salario_base_de_calculo * (12 - mes_inicio + 1)
-        total_deducoes_list[0]          = total_deducoes          * (12 - mes_inicio + 1)
-
-        for i in range(1, (anos_vigencia_aux - 1), 1):
-
-            salario_liquido_list[i]         = salario_liquido         * 12
-            irrf_recolhido_list[i]          = irrf_recolhido          * 12
-            salario_base_list[i]            = salario_base            * 12
-            adicoes_list[i]                 = adicoes                 * 12
-            salario_base_de_calculo_list[i] = salario_base_de_calculo * 12
-            total_deducoes_list[i]          = total_deducoes          * 12
-
-        salario_liquido_list[anos_vigencia_aux - 1]         = salario_liquido         * mes_fim
-        irrf_recolhido_list[anos_vigencia_aux - 1]          = irrf_recolhido          * mes_fim
-        salario_base_list[anos_vigencia_aux - 1]            = salario_base            * mes_fim
-        adicoes_list[anos_vigencia_aux - 1]                 = adicoes                 * mes_fim
-        salario_base_de_calculo_list[anos_vigencia_aux - 1] = salario_base_de_calculo * mes_fim
-        total_deducoes_list[anos_vigencia_aux - 1]          = total_deducoes          * mes_fim
-
-    return salario_liquido_list, irrf_recolhido_list, salario_base_list, adicoes_list, salario_base_de_calculo_list, total_deducoes_list
-    
-def preparar_relatorio(salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido):
-    
-    if(anos_vigencia_aux == 1):
-
-        salario_base = salario_base * (mes_fim - mes_inicio + 1)
-        salario_bruto = salario_bruto * (mes_fim - mes_inicio + 1) 
-        salario_base_de_calculo = salario_base_de_calculo * (mes_fim - mes_inicio + 1)
-        salario_liquido = salario_liquido * (mes_fim - mes_inicio + 1)
-        irrf_recolhido  = irrf_recolhido * (mes_fim - mes_inicio + 1)
-        aliquota_ = irrf_recolhido/salario_bruto
-
-    elif(anos_vigencia_aux > 1):
-
-        salario_base = salario_base * ((12 - mes_inicio + 1) + ((anos_vigencia_aux - 2) * 12) + (mes_fim))
-        salario_bruto = salario_bruto * ((12 - mes_inicio + 1) + ((anos_vigencia_aux - 2) * 12) + (mes_fim))
-        salario_base_de_calculo = salario_base_de_calculo * ((12 - mes_inicio + 1) + ((anos_vigencia_aux - 2) * 12) + (mes_fim))
-        salario_liquido = salario_liquido * ((12 - mes_inicio + 1) + ((anos_vigencia_aux - 2) * 12) + (mes_fim))
-        irrf_recolhido  = irrf_recolhido * ((12 - mes_inicio + 1) + ((anos_vigencia_aux - 2) * 12) + (mes_fim))
-        aliquota_ = irrf_recolhido/salario_bruto
-
-    return salario_base, salario_bruto, salario_base_de_calculo, salario_liquido, irrf_recolhido, aliquota_
 
 while True:
 
